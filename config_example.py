373aebc16ae113a74a993194825befa2c4819bfd
Manh Nguyen Dong <dongmanhhust@gmail.com>
2025-02-03 12:53:18 +0700

    Example config file

---
+++ config_example.py
@@ -0,0 +1,12 @@
+class Config:
+    RETRY_TIMES = 3
+    TIMEOUT = 60
+    MAX_PAGINATION = 10
+    SLEEP_TIME = 0.2
+    HEADER = {
+        # Your headers
+    }
+
+    USER_AGENTS = [
+        # Your user agents
+    ]
